f64778401c12e031210616e7e058dcec224460fb
Ezequiel <ezequielezequiel9@gmail.com>
2024-09-13 18:52:11 -0300

    add var env

--- connections/algolia.ts
+++ connections/algolia.ts
@@ -5,5 +5,5 @@ const appID = "HZJ2XKFLQY";
 const apiKey = "ee5aa4e0920458ee4c406f174832adb8";
 
 // Connect and authenticate with your Algolia app
-const client = algoliasearch(appID, apiKey);
+const client = algoliasearch(process.env.APP_ID, process.env.API_KEY);
 export const dbAlgolia = client.initIndex("airtable");
