c86ac982018824665ddec4de4385b085f94bacd1
Ezequiel <ezequielezequiel9@gmail.com>
2024-10-15 15:41:25 -0300

    edit pages/middleware.ts

--- pages/api/middleware.ts
+++ pages/api/middleware.ts
@@ -3,14 +3,12 @@ import { NextResponse, type NextRequest } from "next/server";
 export function middleware(req: NextRequest) {
   const headers = {
     "Access-Control-Allow-Credentials": "true",
-    "Access-Control-Allow-Origin": "*", // Cambia esto si necesitas un origen específico
+    "Access-Control-Allow-Origin": req.headers.get("Origin") || "*", // Permitir solo el origen de la solicitud
     "Access-Control-Allow-Methods": "GET, OPTIONS, PATCH, DELETE, POST, PUT",
     "Access-Control-Allow-Headers":
       req.headers.get("Access-Control-Request-Headers") || "",
-    "Content-Length": "0",
   };
 
-  // Manejar las solicitudes OPTIONS
   if (req.method === "OPTIONS") {
     return new Response(null, {
       status: 204,
@@ -18,7 +16,6 @@ export function middleware(req: NextRequest) {
     });
   }
 
-  // Para otras solicitudes, asegúrate de que también se envíen los headers CORS
   const response = NextResponse.next();
   Object.entries(headers).forEach(([key, value]) => {
     response.headers.set(key, value);
@@ -28,5 +25,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/api/:path*"], // Aplica este middleware a todas las rutas que empiecen con /api/
+  matcher: ["/api/:path*"],
 };
