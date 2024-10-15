621fa6b7a2387785b014e8eb4b6423fc0713af64
Ezequiel <ezequielezequiel9@gmail.com>
2024-10-15 14:07:09 -0300

    edit config next

--- controllers/middleware.ts
+++ controllers/middleware.ts
@@ -27,7 +27,7 @@ export function handlerCORS(callback) {
     await NextCors(req, res, {
       // Options
       methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-      origin: "http://localhost:3000/",
+      origin: "*",
       optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
     });
 
